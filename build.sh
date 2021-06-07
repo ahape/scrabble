b7e3ced67ba9588fc54fd59c58bc8e56e784e51c
Alan Hape <ahape@brightmetrics.com>
2021-06-07 06:55:18 -0700

    Rm libman; update build thing

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-cp * ../scrabble-azure/
+cp -rf * ../scrabble-azure/
 cd ../scrabble-azure
 rm package.json
-rm tslint.config
+rm tsconfig.json
 rm yarn*
+rm build.sh
 rm -rf node_modules/
 rm -rf ts/
