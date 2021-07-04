98f311f8e36d3533f88a7a30c385624f25d71936
Alan Hape <ahape@brightmetrics.com>
2021-07-04 11:22:57 -0700

    Cont w/ new TS structuring

---
+++ scripts/src/pages/chooseteam/index.ts
@@ -0,0 +1,18 @@
+import * as _ from "underscore";
+import * as ko from "knockout";
+import { IGamePlayer } from "../../interfaces/igameplayer";
+
+export class Index {
+    public teamChoice: KnockoutObservable<number> = ko.observable(1);
+    public playersOnTeam: KnockoutComputed<string>;
+
+    public constructor(players: IGamePlayer[]) {
+        const playersByTeam = _.groupBy(players, "team");
+
+        this.playersOnTeam = ko.pureComputed(() => {
+            return _.pluck(playersByTeam[this.teamChoice()], "userName").join(
+                ", "
+            );
+        });
+    }
+}
