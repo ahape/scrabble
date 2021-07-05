83441fd60692200f7b94d69bc677eea27ed64206
Alan Hape <ahape@brightmetrics.com>
2021-07-05 13:41:15 -0700

    Add sq color in Help

--- scripts/src/pages/game/help.ts
+++ scripts/src/pages/game/help.ts
@@ -1,11 +1,12 @@
 import * as ko from "knockout";
-import { constants as sc } from "scrabblecore";
+import { Multiplier, MultiplierType, constants as sc } from "scrabblecore";
 import { freebies } from "../../constants";
 
 class Help {
     public twoLetterWords: string[];
     public qWithoutU: string[];
     public letterValues: Array<[string, number]>;
+    public squareValues: Array<[string, string]>;
 
     public constructor() {
         this.twoLetterWords = freebies.twoLetterWords;
@@ -14,6 +15,25 @@ class Help {
         // TODO: Determine if we even need an UNSET (null) value.
         // The `splice` here is removing it from displaying.
         this.letterValues.splice(0, 1);
+        this.squareValues = this._getSquareValues();
+    }
+
+    private _getSquareValues(): Array<[string, string]> {
+        const ms = [Multiplier.Double, Multiplier.Triple];
+        const mTypes = [MultiplierType.Letter, MultiplierType.Word];
+        const squareValues: Array<[string, string]> = [];
+
+        for (let i = 0; i < mTypes.length; i++) {
+            for (let j = 0; j < ms.length; j++) {
+                squareValues.push([
+                    mTypes[i] + ms[j], // E.g. word2, letter2
+                    // E.g. "2 times the letter's value"
+                    ms[j] + "x the " + mTypes[i] + "'s value",
+                ]);
+            }
+        }
+
+        return squareValues;
     }
 }
 
