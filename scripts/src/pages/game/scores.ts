f1eee40ccf5e541b925205286fae7684f1850ec6
Alan Hape <ahape@brightmetrics.com>
2021-07-18 21:23:22 -0700

    Change endgame logic

--- scripts/src/pages/game/scores.ts
+++ scripts/src/pages/game/scores.ts
@@ -1,5 +1,5 @@
 import * as ko from "knockout";
-import { Game } from "scrabblecore";
+import { Game, getScoresFromActions } from "scrabblecore";
 
 class Scores {
     public scores: KnockoutComputed<string[]>;
@@ -8,14 +8,34 @@ class Scores {
     public teamNumber: number;
 
     public constructor(params: { game: Game; teamNumber: number }) {
-        const status = params.game.currentStatus;
+        const koStatus = params.game.currentStatus;
 
         this.scores = ko.pureComputed(() => {
-            return status().scores.map((s, i) => `Team ${i + 1}: ${s}`);
+            const s = koStatus();
+            if (s.gameOver) {
+                const state = params.game.snapshot();
+                const pScores = getScoresFromActions(
+                    state.actions.slice(0, -1),
+                    params.game.teams
+                );
+                const cScores = getScoresFromActions(
+                    state.actions,
+                    params.game.teams
+                );
+                const scoreDiffs = pScores.map((p, i) => {
+                    var diff = cScores[i] - p;
+                    return diff > 0 ? "+" + diff : diff;
+                });
+                return s.scores.map(
+                    (s, i) =>
+                        `Team ${i + 1}: ${s} <small>[${scoreDiffs[i]}]</small>`
+                );
+            }
+            return koStatus().scores.map((s, i) => `Team ${i + 1}: ${s}`);
         });
 
-        this.teamTurn = ko.pureComputed(() => status().teamTurn);
-        this.isGameOver = ko.pureComputed(() => status().gameOver);
+        this.teamTurn = ko.pureComputed(() => koStatus().teamTurn);
+        this.isGameOver = ko.pureComputed(() => koStatus().gameOver);
         this.teamNumber = params.teamNumber;
     }
 }
